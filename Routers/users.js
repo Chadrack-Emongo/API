a99bcf3f399218443ce128992a83ed0e100a2e13
emongo <emongochadrack@gmail.com>
2024-02-06 20:14:20 +0200

    a error

--- Routers/users.js
+++ Routers/users.js
@@ -1,12 +1,65 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
  
+
 // L'ajout d'un utilisateur
 
 router.get("/", (req, res) => {
-    // const tweets = Tweet.find().sort({ createdAt: -1 });
     const tweets = [];
     res.json(tweets);
+  });
+
+  //tweet
+  router.post("/", (req, res) => {
+    const tweet = new Tweet({
+      text: req.body.text,
+      author: req.body.author
+    });
+  
+    tweet.save((err, tweet) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(tweet);
+      }
+    });
+  });
+
+  //tweet
+  router.put("/:id", (req, res) => {
+    const tweet = Tweet.findById(req.params.id);
+  
+    if (!tweet) {
+      res.status(404).send("Tweet not found");
+    } else {
+      tweet.text = req.body.text;
+      tweet.author = req.body.author;
+      tweet.save((err, tweet) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.json(tweet);
+        }
+      });
+    }
+  });
+
+
+  //tweet
+  router.delete("/:id", (req, res) => {
+    const tweet = Tweet.findById(req.params.id);
+  
+    if (!tweet) {
+      res.status(404).send("Tweet not found");
+    } else {
+      tweet.remove((err, tweet) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.sendStatus(204);
+        }
+      });
+    }
   });
 
 module.exports = router;
