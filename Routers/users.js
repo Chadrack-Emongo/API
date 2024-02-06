7975241c396c4c30679d5019dd62ac73bc1e7a48
emongo <emongochadrack@gmail.com>
2024-02-06 21:45:26 +0200

    ajout du tableau

--- Routers/users.js
+++ Routers/users.js
@@ -1,21 +1,40 @@
 const express = require('express');
 const router = express.Router();
- 
+
 
 // L'ajout d'un utilisateur
 
 router.get("/", (req, res) => {
-    const tweets = [];
-    res.json(tweets);
-  });
+  const tweets = [];
+  res.json(tweets);
+});
 
-  //tweet
-  router.post("/", (req, res) => {
-    const tweet = new Tweet({
-      text: req.body.text,
-      author: req.body.author
-    });
-  
+//tweet
+router.post("/", (req, res) => {
+  const tweets = [
+    { Id: 1, Text: '' },
+    { Id: 2, Text: '' },
+    { Id: 3, Text: '' },
+  ];
+
+  tweet.save((err, tweet) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(tweet);
+    }
+  });
+});
+
+//tweet
+router.put("/:id", (req, res) => {
+  const tweet = Tweet.Id == req.params.id;
+
+  if (!tweet) {
+    res.status(404).send("Tweet not found");
+  } else {
+    tweet.text = req.body.text;
+    tweet.author = req.body.author;
     tweet.save((err, tweet) => {
       if (err) {
         res.status(500).send(err);
@@ -23,43 +42,25 @@ router.get("/", (req, res) => {
         res.json(tweet);
       }
     });
-  });
-
-  //tweet
-  router.put("/:id", (req, res) => {
-    const tweet = Tweet.findById(req.params.id);
-  
-    if (!tweet) {
-      res.status(404).send("Tweet not found");
-    } else {
-      tweet.text = req.body.text;
-      tweet.author = req.body.author;
-      tweet.save((err, tweet) => {
-        if (err) {
-          res.status(500).send(err);
-        } else {
-          res.json(tweet);
-        }
-      });
-    }
-  });
+  }
+});
 
 
-  //tweet
-  router.delete("/:id", (req, res) => {
-    const tweet = Tweet.findById(req.params.id);
-  
-    if (!tweet) {
-      res.status(404).send("Tweet not found");
-    } else {
-      tweet.remove((err, tweet) => {
-        if (err) {
-          res.status(500).send(err);
-        } else {
-          res.sendStatus(204);
-        }
-      });
-    }
-  });
+//tweet
+router.delete("/:id", (req, res) => {
+  // const tweet = Tweet.findById(req.params.id);
+
+  if (!tweet) {
+    res.status(404).send("Tweet not found");
+  } else {
+    tweet.remove((err, tweet) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.sendStatus(204);
+      }
+    });
+  }
+});
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3002;
+const port = 8000;
 const users = require("./Routers/users")
 
 
