c81d9de1ba9ecea12ff5b54b9070b171f879a3dd
emongo <emongochadrack@gmail.com>
2024-02-07 22:01:01 +0200

    fonction du tableau

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 8001;
 const users = require("./Routers/posteRoute")
 
-
+app.use(express.json())
 app.get('/', (req, res) => {
     res.json({ message: 'Bienvenue sur notre API du tweet' });
 });
