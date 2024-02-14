b47b25ff89e1e3086242514f229cae6e4f4aa4bc
emongo <emongochadrack@gmail.com>
2024-02-14 14:42:04 +0200

    la clé etrangere

--- controlers/routeController.js
+++ controlers/routeController.js
@@ -5,6 +5,22 @@ const getRoute = (req, res) => {
 
 // l'authentification via jwt
 
+const privateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIICXQIBAAKBgQCd/+j4Wg52O6ZQKxTllUTJAUg6CeLm4u1WqMzpFSlVDmPnisaU
+R1aYsjB6x3pIXf7c07ssIHCkHsid+spEZ5YYCzBoYSA7lRN4VVPbY0sRyykEyO1J
+pQg1X0JAtXjk/6rYkDPkP+bczPUU0yiDU4J3xpZ20WAfvx1lZeuBXaG6pQIDAQAB
+AoGAUn3ve+NBE7f3EHo90dRKcovp0uugfR3T/t/pBHTY/KCjxpstLPPGIbk/GIuX
+q7rvuwoM+nABf57W7ayn+GkA8r5loQmyXffMu+esajv9eq/tMJiNzsHuGcBsHHCC
+UnzoWZGBB4atSOQqbJHMWRVVTwA0ZREy09E2Goke5TX3eY0CQQDpZJPuhQRUREd3
+clmiDz6au1XJvYLNd6yFzzRLG1rC2h4JHTBzTUTpxtBpXcr9T7shRq76WcIRPqWB
+9sM8m2LHAkEArU3QINnmUqIE0bLG70kT0A7fP3sjHYbTZIyqr3XXJ6DlEctRIDOT
+nK295C1xBDQn6fmx0SUjTKtBVDj86J2LMwJBALNnjdM2Soqf5bOqob3SLBRy1lH6
+K/bW/DXrfc4SG+VWFup4loq7JbLyEbg80dGAHV9J3y0wG953om/RiGgpJXcCQQCQ
+CI/l/bOTEdz3e6Ii1ZWF4hZhaDHzJE8kzlQ9p6693NcFRvmRzcvs9+lObwXy/HX/
+2+qRbFA+NEcDfKmVmvpJAkAG7aYxbo9RjZ6KRvU03M+SANjtwZgdfR9JENZ1GROR
+cR1lQ+RURfkE4U+Jw49qwirxuuWo89Kr9SJRwJEMMdZd
+-----END RSA PRIVATE KEY-----`
+
 const authentificationJWT = (req, res, next) => {
   const token = req.header('Authorization');
 
